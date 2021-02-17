c5fba313aa157651a32e9c60db28af1f070111cb
Альбина Бийболатова <albinabijbolatova@MacBook-Air-Albina.local>
2021-02-17 23:14:00 +0500

    First Commit

---
+++ #src/js/files/script.js
@@ -0,0 +1,108 @@
+// функция добавления изображения с расширением webp
+function testWebP(callback) {
+	var webP = new Image();
+	webP.onload = webP.onerror = function () {
+		callback(webP.height == 2);
+	};
+	webP.src = "data:image/webp;base64,UklGRjoAAABXRUJQVlA4IC4AAACyAgCdASoCAAIALmk0mk0iIiIiIgBoSygABc6WWgAA/veff/0PP8bA//LwYAAA";
+}
+testWebP(function (support) {
+	if (support == true) {
+		document.querySelector('html').classList.add('_webp');
+	} else {
+		document.querySelector('html').classList.add('_no-webp');
+	}
+});
+
+$(document).ready(function() {
+
+    // Плавная прокрутка по якорям
+    $('a.menu__list-link,a.offer__btn').click(function() {
+        var elementClick = $(this).attr('href')
+        var destination = $(elementClick).offset().top;
+        jQuery('html:not(:animated),body:not(:animated)').animate({
+        scrollTop: destination
+        }, 800);
+        return false;
+    });
+	// Обработчик бургер-меню
+    $('.menu__burger').click(function(event) {
+        $('.menu__burger,.menu').toggleClass('_active');
+		$('body').toggleClass('lock');
+    });
+    $('a.menu__list-link').click(function(event) {
+        $('.menu__burger,.menu').toggleClass('_active');
+		$('body').toggleClass('lock');
+    });
+
+});
+// Валидация и отправка форм
+document.addEventListener('DOMContentLoaded', function () {
+	const form = document.getElementById('form');
+	form.addEventListener('submit', formSend);
+
+	async function formSend(e) {
+		e.preventDefault();
+
+		let error = formValidate(form);
+
+		let formData = new FormData(form);
+
+		if (error === 0) {
+			form.classList.add('_sending');
+			let response = await fetch('sendmail.php', {
+				method: 'POST',
+				body: formData
+			});
+			if (response.ok) {
+				let result = await response.json();
+				alert(result.message);
+				formPreview.innerHTML = '';
+				form.reset();
+				form.classList.remove('_sending');
+			} else {
+				alert("Ошибка");
+				form.classList.remove('_sending');
+			}
+		} else {
+			alert('Заполните обязательные поля');
+		}
+
+	}
+
+
+	function formValidate(form) {
+		let error = 0;
+		let formReq = document.querySelectorAll('._req');
+
+		for (let index = 0; index < formReq.length; index++) {
+			const input = formReq[index];
+			formRemoveError(input);
+
+			if (input.classList.contains('_tel')) {
+				if (telTest(input)) {
+					formAddError(input);
+					error++;
+				}
+			} else {
+				if (input.value === '') {
+					formAddError(input);
+					error++;
+				}
+			}
+		}
+		return error;
+	}
+	function formAddError(input) {
+		input.parentElement.classList.add('_error');
+		input.classList.add('_error');
+	}
+	function formRemoveError(input) {
+		input.parentElement.classList.remove('_error');
+		input.classList.remove('_error');
+	}
+	//Функция теста email
+	function telTest(input) {
+        return !/^\d[\d\(\)\ -]{4,14}\d$/.test(input.value);
+    }
+});
